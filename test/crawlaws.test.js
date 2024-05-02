ab8be80241dbf2057b6a55b4f07773b60f0feada
Jackson Davis <65826384+itsthemoon@users.noreply.github.com>
2024-05-02 17:15:38 -0400

    updating server thingy god please work

--- test/crawlaws.test.js
+++ test/crawlaws.test.js
@@ -1,24 +1,22 @@
-global.nodeConfig = { ip: '127.0.0.1', port: 8070 };
+global.nodeConfig = { ip: '172.31.0.115', port: 8070 };
 const { Console } = require('console');
 const distribution = require('../distribution');
 const id = distribution.util.id;
-
-//groups added dynamically 
-const {
-    server,
-    crawlerGroup,
-    urlExtractGroup,
-    stringMatchGroup,
-    invertGroup,
-    reverseLinkGroup,
-    compactTestGroup,
-    memTestGroup,
-    outTestGroup
-} = require('../api/groupBuilder');
-
-const groupsTemplate = require('../distribution/all/groups');
 const fs = require('fs');
 const path = require('path');
+const groupsFilePath = path.join(__dirname, '..', 'distribution', 'util', 'groups.txt');
+jest.setTimeout(600000)
+
+//groups added dynamically 
+const urlExtractGroup = {};
+const stringMatchGroup = {};
+const invertGroup = {};
+const reverseLinkGroup = {};
+const compactTestGroup = {};
+const memTestGroup = {};
+const outTestGroup = {};
+
+const groupsTemplate = require('../distribution/all/groups');
 
 global.fetch = require('node-fetch');
 
@@ -29,7 +27,6 @@ global.fetch = require('node-fetch');
    running in is the actual jest process
 */
 let localServer = null;
-
 /*
     The local node will be the orchestrator.
 */
@@ -38,94 +35,52 @@ const n1 = { ip: '127.0.0.1', port: 8110 };
 const n2 = { ip: '127.0.0.1', port: 8111 };
 const n3 = { ip: '127.0.0.1', port: 8112 };
 
+function getNodesFromFile() {
+    const groupsData = fs.readFileSync(groupsFilePath, 'utf8');
+    const nodes = {};
+
+    groupsData.split('\n').forEach((line) => {
+        if (line.trim() !== '') {
+            const [nodeId, nodeData] = line.split(': ');
+            nodes[nodeId] = JSON.parse(nodeData);
+        }
+    });
+
+    return nodes;
+}
+
+const crawlerGroup = getNodesFromFile();
+
 beforeAll((done) => {
     /* Stop the nodes if they are running */
-
-    // Wait for the worker nodes to join the groups
-    const checkNodesJoined = () => {
-        if (
-            Object.keys(crawlerGroup).length === 10 &&
-            Object.keys(urlExtractGroup).length === 10 &&
-            Object.keys(stringMatchGroup).length === 10 &&
-            Object.keys(invertGroup).length === 10 &&
-            Object.keys(reverseLinkGroup).length === 10 &&
-            Object.keys(compactTestGroup).length === 10 &&
-            Object.keys(memTestGroup).length === 10 &&
-            Object.keys(outTestGroup).length === 10
-        ) {
-            done();
-        } else {
-            setTimeout(checkNodesJoined, 1000);
-        }
-    };
-
-    checkNodesJoined();
-
     distribution.node.start((server) => {
         localServer = server;
 
-        const crawlerConfig = { gid: 'crawler' };
+        const crawlerConfig = { gid: 'crawler', hash: global.distribution.util.id.consistentHash };
         groupsTemplate(crawlerConfig).put(
             crawlerConfig, crawlerGroup, (e, v) => {
-                const urlExtractConfig = { gid: 'urlExtract' };
-                groupsTemplate(urlExtractConfig).put(
-                    urlExtractConfig, urlExtractGroup, (e, v) => {
-                        const invertConfig = { gid: 'invert' };
-                        groupsTemplate(invertConfig).put(
-                            invertConfig, invertGroup, (e, v) => {
-                                const compactTestConfig = { gid: 'compactTest' };
-                                groupsTemplate(compactTestConfig).put(
-                                    compactTestConfig, compactTestGroup,
-                                    (e, v) => {
-                                        const memTestConfig = { gid: 'memTest' };
-                                        groupsTemplate(memTestConfig).put(
-                                            memTestConfig,
-                                            memTestGroup,
-                                            (e, v) => {
-                                                const outTestConfig =
-                                                    { gid: 'outTest' };
-                                                groupsTemplate(outTestConfig).put(
-                                                    outTestConfig,
-                                                    outTestGroup,
-                                                    (e, v) => {
-                                                        const stringMatchConfig =
-                                                            { gid: 'stringMatch' };
-                                                        groupsTemplate(stringMatchConfig).put(
-                                                            stringMatchConfig,
-                                                            stringMatchGroup,
-                                                            (e, v) => {
-                                                                const reverseLinkConfig =
-                                                                    { gid: 'reverseLink' };
-                                                                groupsTemplate(
-                                                                    reverseLinkConfig).put(
-                                                                        reverseLinkConfig,
-                                                                        reverseLinkGroup,
-                                                                        (e, v) => {
-                                                                            done();
-                                                                        });
-                                                            });
-                                                    });
-                                            });
-                                    });
-                            });
-                    });
             });
     });
 });
 
 afterAll((done) => {
-    let remote = { service: 'status', method: 'stop' };
-    remote.node = n1;
-    distribution.local.comm.send([], remote, (e, v) => {
-        remote.node = n2;
+    const nodes = Object.values(crawlerGroup);
+    const stopNode = (index) => {
+        if (index >= nodes.length) {
+            localServer.close();
+            done();
+            return;
+        }
+        const remote = {
+            service: 'status',
+            method: 'stop',
+            node: nodes[index]
+        };
         distribution.local.comm.send([], remote, (e, v) => {
-            remote.node = n3;
-            distribution.local.comm.send([], remote, (e, v) => {
-                localServer.close();
-                done();
-            });
+            stopNode(index + 1);
         });
-    });
+    };
+    stopNode(0);
 });
 
 test('all.mr:crawler!', (done) => {
@@ -208,7 +163,6 @@ test('all.mr:crawler!', (done) => {
             cntr++;
             // Once we are done, run the map reduce
             if (cntr === dataset.length) {
-                doMapReduce();
             }
         });
     });
